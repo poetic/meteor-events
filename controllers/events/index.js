8505210885f876034869988b47453d5965bd40d1
Leonidez Acosta <leonidez@poeticsystems.com>
2015-04-18 12:10:06 -0500

    events/index controller

--- controllers/events/index.js
+++ controllers/events/index.js
@@ -1,2 +1,18 @@
 EventsIndexController = ApplicationController.extend({
+  data: {
+    upcomingEvents: function(){
+      var currentTime = new Date()
+
+      return Events.find({
+        date: { $gte: currentTime }
+      });
+    },
+    pastEvents: function(){
+      var currentTime = new Date()
+
+      return  Events.find({
+        date: { $lt: currentTime }
+      });
+    }
+  },
 });
