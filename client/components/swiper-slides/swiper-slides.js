84314f08504eb4e66253ae26d2cc36ee99c3ff09
Leonidez Acosta <leonidez@poeticsystems.com>
2015-04-18 18:00:47 -0500

    update url when transitioning between slides

--- client/components/swiper-slides/swiper-slides.js
+++ client/components/swiper-slides/swiper-slides.js
@@ -1,16 +1,34 @@
-Template.swiperSlides.rendered = function(){
-  var param = this.data.param;
-  var options = this.data.options;
+Template.swiperSlides.created = function(){
+  //console.log(history.state)
+  //var param = this.data.param;
 
-  var defaults = {
-    onTransitionEnd: function(e){
-      if (param) {
-        console.log(e.activeIndex);
-      }
-    }
-  };
+  //if (routeHasQueryParam(param)) {
+    //this.data.initialSlideOverride = Router.current().params.query[param];
+  //}
+};
+
+Template.swiperSlides.rendered = function(){
+  var options = this.data.options;
+  var defaults = { onTransitionEnd: onTransitionEnd.bind(this) };
+
+  //if (_.has(this.data, 'initialSlideOverride')) {
+    //defaults.initialSlide = this.data.initialSlideOverride;
+  //}
 
   var swiperOptions = _.extend(options, defaults);
 
   this.$('.swiper-container').swiper(swiperOptions);
+};
+
+function onTransitionEnd (event){
+  var param = this.data.param;
+  var newState = {}
+
+  if (history.state[param] !== event.activeIndex) {
+    newState[param] = event.activeIndex;
+    history.pushState(newState, '',  '?' + param + '=' + event.activeIndex);
+  }
+
+  //var query = { query: param + '=' + e.activeIndex };
+  //Router.go(currentRoute, {}, query);
 };
