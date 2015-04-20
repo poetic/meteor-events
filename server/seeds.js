16267d5c9948e77ae6490806a92f3a88e52afec7
Leonidez Acosta <leonidez@poeticsystems.com>
2015-04-19 22:45:03 -0500

    seeds for event collection

--- server/seeds.js
+++ server/seeds.js
@@ -1,28 +1,93 @@
 Meteor.startup(function(){
   if (!Events.find().count()) {
-    _(5).times(function(){
+    _(10).times(function(){
       Events.insert({
+        coverPhoto: faker.image.imageUrl(),
         title: faker.name.findName(),
-        date: faker.date.future(),
-        image: faker.image.imageUrl(),
-      });
-    });
+        description: faker.lorem.sentences(),
+        isPlusOne: true,
 
-    _(5).times(function(){
-      Events.insert({
-        title: faker.name.findName(),
-        date: faker.date.past(),
-        image: faker.image.imageUrl(),
-      });
-    });
-  }
+        hosts: [{
+          name: faker.name.findName(),
+          role: faker.hacker.adjective(),
+          email: faker.internet.email(),
+          phone: faker.phone.phoneNumber(),
+        }, {
+          name: faker.name.findName(),
+          role: faker.hacker.adjective(),
+          email: faker.internet.email(),
+          phone: faker.phone.phoneNumber(),
+        }],
 
-  if (!Users.find().count()) {
-    _(5).times(function(){
-      Users.insert({
-        firstName: faker.name.firstName(),
-        lastName: faker.name.lastName(),
-        email: faker.internet.email(),
+        activities: [{
+          title: faker.name.findName(),
+          startTime: faker.date.past(),
+          endTime: new Date(),
+          timeZone: 'central',
+
+          address: {
+            locationName: faker.company.companyName(),
+            address1: faker.address.streetAddress(),
+            address2: faker.address.secondaryAddress(),
+            city: faker.address.city(),
+            state: faker.address.state(),
+            zipCode: faker.address.zipCode(),
+          },
+
+          attire: faker.lorem.sentence(),
+          transit: faker.lorem.sentence(),
+          notes: faker.lorem.sentence(),
+        }, {
+          title: faker.name.findName(),
+          startTime: faker.date.past(),
+          endTime: new Date(),
+          timeZone: 'central',
+
+          address: {
+            locationName: faker.company.companyName(),
+            address1: faker.address.streetAddress(),
+            address2: faker.address.secondaryAddress(),
+            city: faker.address.city(),
+            state: faker.address.state(),
+            zipCode: faker.address.zipCode(),
+          },
+
+          attire: faker.lorem.sentence(),
+          transit: faker.lorem.sentence(),
+          notes: faker.lorem.sentence(),
+        }, {
+          title: faker.name.findName(),
+          startTime: faker.date.past(),
+          endTime: new Date(),
+          timeZone: 'central',
+
+          address: {
+            locationName: faker.company.companyName(),
+            address1: faker.address.streetAddress(),
+            address2: faker.address.secondaryAddress(),
+            city: faker.address.city(),
+            state: faker.address.state(),
+            zipCode: faker.address.zipCode(),
+          },
+
+          attire: faker.lorem.sentence(),
+          transit: faker.lorem.sentence(),
+          notes: faker.lorem.sentence(),
+        }],
+
+        comments: [{
+          body: faker.lorem.sentence(),
+          imageUrl: faker.image.imageUrl(),
+        }, {
+          body: faker.lorem.sentence(),
+          imageUrl: faker.image.imageUrl(),
+        }, {
+          body: faker.lorem.sentence(),
+          imageUrl: faker.image.imageUrl(),
+        }, {
+          body: faker.lorem.sentence(),
+          imageUrl: faker.image.imageUrl(),
+        }],
       });
     });
   }
