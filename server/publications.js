a3c784b6ef8a06a7e4cfd7b64cb2d4e12e8d755b
Leonidez Acosta <leonidez@poeticsystems.com>
2015-04-21 17:06:48 -0500

    remove auto publish package and define publications

---
+++ server/publications.js
@@ -0,0 +1,11 @@
+Meteor.publish('users', function(){
+  return Users.find();
+});
+
+Meteor.publish('events', function(){
+  return Events.find();
+});
+
+Meteor.publish('event', function(eventId){
+  return Events.find(eventId)
+});
