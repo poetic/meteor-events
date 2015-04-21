a5ffafec10c54a218478eb9d3659f165e2eca6bf
Leonidez Acosta <leonidez@poeticsystems.com>
2015-04-21 00:07:45 -0500

    event/planning

---
+++ client/views/events/show/planning.js
@@ -0,0 +1,34 @@
+Template.EventsShowPlanning.created = function(){
+  if (!this.data) { this.data = {} }
+
+  this.data.linksOptions = {
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    slideToClickedSlide: true,
+    touchRatio: 0.35,
+    initialSlide: 2,
+  };
+
+  this.data.slidesOptions = {
+    initialSlide: 2,
+  };
+};
+
+Template.EventsShowPlanning.rendered = function(){
+  var swiper1 = this.$('.swiper-container')[0].swiper;
+  var swiper2 = this.$('.swiper-container')[1].swiper;
+
+  swiper1.params.control = swiper2;
+  swiper2.params.control = swiper1;
+};
+
+Template.EventsShowPlanning.helpers({
+  isPlanner: function(){
+    if (this.currentEvent && this.currentEvent()) {
+      return Router.current().params._id === this.currentEvent().user;
+    }
+  },
+  commentedAt: function(){
+
+  }
+});
---
+++ controllers/events/show/planning.js
@@ -0,0 +1,12 @@
+EventsShowPlanningController = ApplicationController.extend({
+  data: function(){
+    var routeParams = Router.current().params;
+    var eventId = routeParams.event_id;
+
+    return {
+      currentEvent: function(){
+        return Events.findOne(eventId);
+      }
+    };
+  }
+})
