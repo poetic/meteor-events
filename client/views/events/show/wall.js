e16aa66e611bdc3d6721f23a43e8d0cb5cd9fff1
Leonidez Acosta <leonidez@poeticsystems.com>
2015-04-21 01:53:26 -0500

    helpers to show username and time comment was made

--- client/views/events/show/wall.js
+++ client/views/events/show/wall.js
@@ -8,10 +8,22 @@ Template.EventsShowWall.events({
     var comment = {
       user: routeParams._id,
       body: event.target.comment.value,
+      created: new Date(),
     };
 
     Events.update({ _id: eventId }, { $push: { comments: comment } });
 
     event.target.comment.value = '';
+  }
+});
+
+Template.EventsShowWall.helpers({
+  commentedAt: function(){
+    return moment(this.created).fromNow();
+  },
+  username: function(){
+    var userId = this.user;
+
+    return Users.findOne(userId).fullName;
   }
 });
