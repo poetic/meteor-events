e06fe374df098bcd92aeba6e385841886587b4b1
Leonidez Acosta <leonidez@poeticsystems.com>
2015-04-24 17:01:57 -0500

    actions to make phone calls and create emails/sms

---
+++ client/views/events/show/host.js
@@ -0,0 +1,21 @@
+Template.EventsShowHost.events({
+  'click .mdi-communication-email': function(event){
+    stopEvent(event);
+    window.open('mailto:' + this.email, '_system');
+  },
+
+  'click .mdi-communication-phone': function(event){
+    stopEvent(event);
+    location.href = 'tel:' + this.phone;
+  },
+
+  'click .mdi-communication-message': function(event){
+    stopEvent(event);
+    window.open('sms:' + this.phone, '_system');
+  },
+});
+
+function stopEvent (event){
+  event.preventDefault();
+  event.stopPropagation();
+};
