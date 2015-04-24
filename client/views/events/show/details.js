437143733d50322755aa196a8eea326318727367
Leonidez Acosta <leonidez@poeticsystems.com>
2015-04-24 12:16:50 -0500

    set width of google map based on width of viewport

--- client/views/events/show/details.js
+++ client/views/events/show/details.js
@@ -1,3 +1,11 @@
+Template.EventsShowDetails.created = function(){
+  this.state = new ReactiveDict();
+};
+
+Template.EventsShowDetails.rendered = function(){
+  this.state.set('mapWidth', $('.map-container').width());
+};
+
 Template.EventsShowDetails.events({
   'click .more-details': function(event){
     event.preventDefault();
@@ -6,18 +14,20 @@ Template.EventsShowDetails.events({
     var el = event.target;
 
       // remove the 40px top/bottom padding of the li
-    var parentHeight = el.parentNode.scrollHeight - 40;
+    var parentHeight = el.parentNode.parentNode.scrollHeight - 40;
 
     if ($(el).hasClass('expanded')) {
       $(el).removeClass('expanded');
 
       $(el).velocity('reverse');
-      $(el).parent().velocity('reverse');
+      $(el).parent().parent().velocity('reverse');
     } else {
       $(el).addClass('expanded');
 
       $.Velocity(el, { rotateZ: '180deg' }, { duration: 200 });
-      $.Velocity($(el).parent(), { height: parentHeight }, { duration: 200 });
+      $.Velocity($(el).parent().parent(),
+        { height: parentHeight }, { duration: 200 }
+      );
     }
   },
 });
@@ -34,4 +44,35 @@ Template.EventsShowDetails.helpers({
   formattedEndTime: function(){
     return moment(this.endTime).format('h:mm a');
   },
+
+  staticMapUrl: function(address){
+    var mapHeight = '100';
+    var mapWidth = Template.instance().state.get('mapWidth');
+    var fullAddress = encodeFullAddress(this.address);
+
+    if (mapWidth) {
+      return buildMapUrl(fullAddress, mapHeight, mapWidth);
+    }
+  }
 });
+
+function encodeFullAddress (address){
+  var fullAddress = (
+    address.address1 + ' ' +
+    address.address2 + ' ' +
+    address.city + ', ' +
+    address.state + ' ' +
+    address.zipCode
+  );
+
+  return encodeURIComponent(fullAddress);
+};
+
+function buildMapUrl (address, mapHeight, mapWidth){
+  var url = Meteor.settings.public.googleApiUrl +
+    '?markers=' + address +
+    '&size=' + mapWidth + 'x' + mapHeight +
+    '&key=' + Meteor.settings.public.googleApiKey;
+
+  return url;
+};
