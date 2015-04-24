9e62817f9ba09d9fa0e21550bb33d24245765d36
Leonidez Acosta <leonidez@poeticsystems.com>
2015-04-24 17:00:42 -0500

    add inappbrowser plugin and add mailto, sms protocols to whitelist

--- mobile-config.js
+++ mobile-config.js
@@ -2,3 +2,5 @@ App.accessRule('*://lorempixel.com/*');
 App.accessRule('*.google.com/*');
 App.accessRule('*.googleapis.com/*');
 App.accessRule('*.gstatic.com/*');
+App.accessRule('mailto:*', { launchExternal: true });
+App.accessRule('sms:*', { launchExternal: true });
