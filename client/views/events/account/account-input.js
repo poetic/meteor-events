db56ba6cb29ea230a70b5aeb637b9b6ef2aca61d
Chun Yang <yang2007chun@gmail.com>
2015-04-29 14:12:54 -0500

    refactor: move account input into a new template

--- client/views/events/account/account-input.js
+++ client/views/events/account/account-input.js
@@ -1,3 +1,15 @@
-Template.AccountInput.helpers({
+/**
+ * params:
+ *  model: {Hash}
+ *  name: {String}
+ */
 
+Template.AccountInput.helpers({
+ _label: function() {
+   var defalutLabel = s.underscored(this.name).replace(/_/g, ' ').toUpperCase();
+   return this.label || defalutLabel;
+ },
+ _value: function() {
+   return this.model[this.name];
+ },
 });
