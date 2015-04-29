40812dba0b92d2826271ef8261f649bb380bef6a
Chun Yang <yang2007chun@gmail.com>
2015-04-29 13:40:26 -0500

    save account and close account page after save

--- client/views/events/account.js
+++ client/views/events/account.js
@@ -1,5 +1,13 @@
+Template.Account.helpers({
+  profile: function() {
+    return Meteor.user().profile;
+  },
+});
+
 Template.Account.events({
   'submit form': function(event) {
+    event.preventDefault();
+
     Meteor.users.update(Meteor.userId(), {
       $set: {
         'profile.fullName':       event.target.fullName.value,
@@ -8,11 +16,16 @@ Template.Account.events({
         'profile.phone':          event.target.phone.value
       }
     });
+
+    closeAccount();
+
     return false;
   },
-  'click .close': function() {
-    $('#account').velocity('reverse', function() {
-      $('#account').css('display', 'none');
-    });
-  },
+  'click .close': closeAccount,
 });
+
+function closeAccount() {
+  $('#account').velocity('reverse', function() {
+    $('#account').css('display', 'none');
+  });
+}
---
+++ client/views/events/account/account-input.js
@@ -0,0 +1,3 @@
+Template.AccountInput.helpers({
+
+});
