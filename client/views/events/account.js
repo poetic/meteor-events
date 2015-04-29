9b927128b13c496dcfd6b6e0f995873921609442
Chun Yang <yang2007chun@gmail.com>
2015-04-29 12:57:05 -0500

    save user info

--- client/views/events/account.js
+++ client/views/events/account.js
@@ -1,5 +1,13 @@
 Template.Account.events({
   'submit form': function(event) {
+    Meteor.users.update(Meteor.userId(), {
+      $set: {
+        'profile.fullName':       event.target.fullName.value,
+        'profile.email':          event.target.email.value,
+        'profile.secondaryEmail': event.target.secondaryEmail.value,
+        'profile.phone':          event.target.phone.value
+      }
+    });
     return false;
   },
   'click .close': function() {
